53a9fe0c832b75b2629935cfa825756959a285e8
kesogan <lambertfernanda@yahoo.com>
2021-12-24 09:35:11 +0100

    first commit

---
+++ person_detect.py
@@ -0,0 +1,204 @@
+
+import numpy as np
+import time
+from openvino.inference_engine import IECore
+import os
+import cv2
+import argparse
+import sys
+
+
+class Queue:
+    '''
+    Class for dealing with queues
+    '''
+    def __init__(self):
+        self.queues=[]
+
+    def add_queue(self, points):
+        self.queues.append(points)
+
+    def get_queues(self, image):
+        for q in self.queues:
+            x_min, y_min, x_max, y_max=q
+            frame=image[y_min:y_max, x_min:x_max]
+            yield frame
+    
+    def check_coords(self, coords):
+        d={k+1:0 for k in range(len(self.queues))}
+        for coord in coords:
+            for i, q in enumerate(self.queues):
+                if coord[0]>q[0] and coord[2]<q[2]:
+                    d[i+1]+=1
+        return d
+
+
+class PersonDetect:
+    '''
+    Class for the Person Detection Model.
+    '''
+
+    def __init__(self, model_name, device, threshold=0.60):
+        self.model_weights=model_name+'.bin'
+        self.model_structure=model_name+'.xml'
+        self.device=device
+        self.threshold=threshold
+
+        try:
+            self.core = IECore()
+            self.model=self.core.read_network(model=self.model_structure, weights=self.model_weights)
+        except Exception as e:
+            raise ValueError("Could not Initialise the network. Have you enterred the correct model path?")
+
+        self.input_name=next(iter(self.model.inputs))
+        self.input_shape=self.model.inputs[self.input_name].shape
+        self.output_name=next(iter(self.model.outputs))
+        self.output_shape=self.model.outputs[self.output_name].shape
+        
+    def is_all_layers_supported(self):
+        
+        all_supported = True
+        supported_layers = self.core.query_network(network=self.model, device_name=self.device)
+        unsupported_layers = [l for l in self.model.layers.keys() if l not in supported_layers]
+        if len(unsupported_layers) != 0:
+            all_supported = False
+
+        return all_supported, unsupported_layers
+    
+    def load_model(self):
+        self.exec_network = self.core.load_network(network = self.model, device_name = self.device)
+        all_supported,unsupported_layers = self.is_all_layers_supported()
+        if not all_supported:
+            print("Unsupported layers found: {}".format(unsupported_layers))
+            
+        
+    def predict(self, image, initial_w, initial_h):
+        input_dict = self.preprocess_input(image)
+        
+        self.exec_network.infer(input_dict)
+        outputs = self.exec_network.requests[0].outputs[self.output_name]
+        coords = self.preprocess_outputs(outputs, initial_w, initial_h)
+        image = self.draw_outputs(coords, image)
+        return coords, image
+    
+    def draw_outputs(self, coords, image):
+        
+        for coord in coords:
+            x_min, y_min, x_max, y_max = coord
+            p1 = (x_min,y_min)
+            p2 = (x_max,y_max)
+            image = cv2.rectangle(image, p1, p2, (0, 255, 0), 3)
+        return image
+        
+    def preprocess_outputs(self, outputs, initial_w, initial_h):
+        
+        boxes = outputs[0,0]
+        coords = []
+        for box in boxes:
+            coord = []
+            if box[2] > self.threshold:
+                x_min = int(box[3]*initial_w)
+                y_min = int(box[4]*initial_h)
+                x_max = int(box[5]*initial_w)
+                y_max = int(box[6]*initial_h)
+                coord = [x_min, y_min, x_max, y_max]
+                coords.append(coord)    
+        return coords
+    
+    def preprocess_input(self, image):
+        
+        w = self.input_shape[3]
+        h = self.input_shape[2]
+        image=cv2.resize(image, (w,h), interpolation = cv2.INTER_AREA)
+        image=np.moveaxis(image, -1, 0)
+        input_dict = {self.input_name:image}
+        return input_dict
+        
+def main(args):
+    model=args.model
+    device=args.device
+    video_file=args.video
+    max_people=args.max_people
+    threshold=args.threshold
+    output_path=args.output_path
+
+    start_model_load_time=time.time()
+    pd= PersonDetect(model, device, threshold)
+    pd.load_model()
+    total_model_load_time = time.time() - start_model_load_time
+
+    queue=Queue()
+    
+    try:
+        queue_param=np.load(args.queue_param)
+        for q in queue_param:
+            queue.add_queue(q)
+    except:
+        print("error loading queue param file")
+
+    try:
+        cap=cv2.VideoCapture(video_file)
+    except FileNotFoundError:
+        print("Cannot locate video file: "+ video_file)
+    except Exception as e:
+        print("Something else went wrong with the video file: ", e)
+    
+    initial_w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    initial_h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    video_len = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+    out_video = cv2.VideoWriter(os.path.join(output_path, 'output_video.mp4'), cv2.VideoWriter_fourcc(*'avc1'), fps, (initial_w, initial_h), True)
+    
+    counter=0
+    start_inference_time=time.time()
+
+    try:
+        while cap.isOpened():
+            ret, frame=cap.read()
+            if not ret:
+                break
+            counter+=1
+            
+            coords, image= pd.predict(frame,initial_w,initial_h)
+            num_people= queue.check_coords(coords)
+            print(f"Total People in frame = {len(coords)}")
+            print(f"Number of people in queue = {num_people}")
+            out_text=""
+            y_pixel=25
+            
+            for k, v in num_people.items():
+                out_text += f"No. of People in Queue {k} is {v} "
+                if v >= int(max_people):
+                    out_text += f" Queue full; Please move to next Queue "
+                cv2.putText(image, out_text, (15, y_pixel), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 0), 2)
+                out_text=""
+                y_pixel+=40
+            out_video.write(image)
+            
+        total_time=time.time()-start_inference_time
+        total_inference_time=round(total_time, 1)
+        fps=counter/total_inference_time
+
+        with open(os.path.join(output_path, 'stats.txt'), 'w') as f:
+            f.write(str(total_inference_time)+'\n')
+            f.write(str(fps)+'\n')
+            f.write(str(total_model_load_time)+'\n')
+
+        cap.release()
+        cv2.destroyAllWindows()
+    except Exception as e:
+        print("Could not run Inference: ", e)
+
+if __name__=='__main__':
+    parser=argparse.ArgumentParser()
+    parser.add_argument('--model', required=True)
+    parser.add_argument('--device', default='CPU')
+    parser.add_argument('--video', default=None)
+    parser.add_argument('--queue_param', default=None)
+    parser.add_argument('--output_path', default='/results')
+    parser.add_argument('--max_people', default=2)
+    parser.add_argument('--threshold', default=0.60)
+    
+    args=parser.parse_args()
+
+    main(args)
